e898af03aae40ccbe2ddc68fb53de78d3cc605c0
SjoerdJakobs <18284@ma-web.nl>
2016-06-06 11:41:55 +0200

    ....
    ....

--- Hergebruik2/Assets/UIHealth.cs
+++ Hergebruik2/Assets/UIHealth.cs
@@ -7,14 +7,30 @@ public class UIHealth : MonoBehaviour
     [SerializeField] private GameObject player;
     private PlayerHealth playerHealth;
     private Image healthBar;
+    [SerializeField]
+    private Transform timeBar;
+    [SerializeField]
+    private float startTimer;
+    private float timer;
 
     void Awake()
     {
         playerHealth = player.GetComponent<PlayerHealth>();
         healthBar = GetComponent<Image>();
         playerHealth.OnPlayerDamage += ChangeHealthBar;
+        timer = startTimer;
+        StartCoroutine(timerCo());
     }
-
+    IEnumerator timerCo()
+    {
+        while (true)
+        {
+            timer--;
+            float xscale = timer / startTimer;
+            timeBar.localScale = new Vector3(xscale, timeBar.localScale.y, timeBar.localScale.z);
+            yield return new WaitForSeconds(1);
+        }
+    }
     void ChangeHealthBar()
     {
         healthBar.fillAmount -= 0.2f;
